73549e6e8699345980ce2fa05f67be073ee3203e
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-14 12:30:59 -0300

    closing in on a solution

--- src/app/(routes)/svg/page.tsx
+++ src/app/(routes)/svg/page.tsx
@@ -2,40 +2,59 @@
 
 import { useMemo } from "react";
 
+import * as THREE from "three";
 import { SVGLoader } from "three/examples/jsm/Addons.js";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { Shape } from "@react-three/drei";
-import { useTransition } from "@react-spring/three";
+import { OrbitControls } from "@react-three/drei";
+
+/**
+ * [Closing in on a solution](https://stackoverflow.com/q/77660929/4756173)
+ */
+function Logo() {
+  const svgData = useLoader(SVGLoader, "lvup_icon.svg");
+  const shapes = useMemo(() => {
+    return svgData.paths.map((p) => p.toShapes(true));
+  }, [svgData]);
+
+  return (
+    <mesh
+      scale={0.05}
+      rotation={[1 * Math.PI, 0, 0]}
+      position={[-0.7, 0.5, 0]}
+    >
+      {shapes.map((s, i) => (
+        <extrudeGeometry
+          key={i}
+          args={[
+            s,
+            {
+              depth: 1,
+              bevelEnabled: false,
+              steps: 30,
+            },
+          ]}
+        />
+      ))}
+      <meshPhongMaterial
+        color="red"
+        side={THREE.DoubleSide}
+      />
+    </mesh>
+  );
+}
 
 export default function Svg() {
-  const data = useLoader(
-    SVGLoader,
-    "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/svg/tiger.svg"
-  );
-
-  const shapes = useMemo(
-    () =>
-      data.paths.flatMap((g, index) =>
-        g.toShapes(true).map((shape) => ({
-          shape,
-          color: g.color,
-          index,
-        }))
-      ),
-    [data]
-  );
-
   return (
     <main style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
+        <OrbitControls />
         <ambientLight intensity={0.5} />
         <spotLight
           intensity={0.5}
           position={[300, 300, 4000]}
         />
 
-        {/* @ts-ignore */}
-        {shapes && <shapeGeometry args={[...shapes]} />}
+        <Logo />
       </Canvas>
     </main>
   );
