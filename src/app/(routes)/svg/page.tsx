ee7a26b099e6c1cb5ecc23e73f2a5fa9bedcbf57
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-14 17:57:01 -0300

    getting closer to an extruded SVG

--- src/app/(routes)/svg/page.tsx
+++ src/app/(routes)/svg/page.tsx
@@ -4,41 +4,80 @@ import { useMemo } from "react";
 
 import * as THREE from "three";
 import { SVGLoader } from "three/examples/jsm/Addons.js";
-import { Canvas, useLoader } from "@react-three/fiber";
+import {
+  Canvas,
+  MeshProps,
+  useLoader,
+} from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 /**
  * [Closing in on a solution](https://stackoverflow.com/q/77660929/4756173)
  */
+// function Logo() {
+//   const svgData = useLoader(SVGLoader, "lvup_icon.svg");
+//   const shapes = useMemo(() => {
+//     return svgData.paths.map((p) => p.toShapes(true));
+//   }, [svgData]);
+
+//   return (
+//     <mesh
+//       scale={0.05}
+//       rotation={[1 * Math.PI, 0, 0]}
+//       position={[-0.7, 0.5, 0]}
+//     >
+//       {shapes.map((s, i) => (
+//         <extrudeGeometry
+//           key={i}
+//           args={[
+//             s,
+//             {
+//               depth: 1,
+//               bevelEnabled: false,
+//               steps: 30,
+//             },
+//           ]}
+//         />
+//       ))}
+//       <meshPhongMaterial
+//         color="red"
+//         side={THREE.DoubleSide}
+//       />
+//     </mesh>
+//   );
+// }
+
 function Logo() {
-  const svgData = useLoader(SVGLoader, "lvup_icon.svg");
+  const svgData = useLoader(SVGLoader, "/tiger.svg");
   const shapes = useMemo(() => {
     return svgData.paths.map((p) => p.toShapes(true));
   }, [svgData]);
 
   return (
     <mesh
-      scale={0.05}
-      rotation={[1 * Math.PI, 0, 0]}
-      position={[-0.7, 0.5, 0]}
+      scale={0.01}
+      rotation={[Math.PI, 0, 0]}
+      position={[-1.7, 2.5, 0]}
     >
       {shapes.map((s, i) => (
-        <extrudeGeometry
-          key={i}
-          args={[
-            s,
-            {
-              depth: 1,
-              bevelEnabled: false,
-              steps: 30,
-            },
-          ]}
-        />
+        <mesh key={i} position={[0, 0, 0]}>
+          <extrudeGeometry
+            args={[
+              s,
+              {
+                depth: 5,
+                bevelEnabled: false,
+                steps: 30,
+              },
+            ]}
+          />
+          <meshPhongMaterial
+            attach="material"
+            color={svgData.paths[i].color}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
       ))}
-      <meshPhongMaterial
-        color="red"
-        side={THREE.DoubleSide}
-      />
     </mesh>
   );
 }
