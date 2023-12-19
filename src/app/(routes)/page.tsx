6eb1c6230edcfa1f94770993fda33093ca40e181
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-19 08:55:27 -0300

    reorganizing the main menu

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -5,57 +5,103 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Three.js Lab</h1>
+      <h1>Philippe Fanaro&apos;s Three.js Lab</h1>
 
-      <ul>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
         <li>
-          <Link href="/simple">Simple</Link>
+          Basic Techniques
+          <ul>
+            <li>
+              <Link href="/simple">Simple</Link>
+            </li>
+            <li>
+              <Link href="/spinning-boxes">
+                Spinning Geometries
+              </Link>
+            </li>
+            <li>
+              <Link href="/reflective-plane">
+                Reflective Plane
+              </Link>
+            </li>
+            <li>
+              <Link href="/basic-env-map">
+                Environment Mapping
+              </Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/spinning-boxes">
-            Spinning Geometries
-          </Link>
+          Post-Processing
+          <ul>
+            <li>
+              <Link href="/bloom-filter">Bloom Filter</Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/reflective-plane">
-            Reflective Plane
-          </Link>
+          Extrusion
+          <ul>
+            <li>
+              <Link href="/extrusion">Simple</Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/basic-env-map">
-            Basic Environment Mapping
-          </Link>
+          HTML and Text
+          <ul>
+            <li>
+              <Link href="/text-3d">3D Text</Link>
+            </li>
+            <li>
+              <Link href="/iframe-positioning">
+                iframe Positioning
+              </Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/basic-custom-shader">
-            Basic Custom Shader
-          </Link>
+          Shaders
+          <ul>
+            <li>
+              <Link href="/basic-custom-shader">
+                Basic Custom Shader
+              </Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/decal">Decal</Link>
+          SVG
+          <ul>
+            <li>
+              <Link href="/svg">SVG Loading</Link>
+            </li>
+            <li>
+              <Link href="/svg">SVG to Shape</Link>
+            </li>
+            <li>
+              <Link href="/svg-mesh-3d">SVG Mesh 3D</Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/extrusion">Extrusion</Link>
-        </li>
-        <li>
-          <Link href="/svg">SVG</Link>
-        </li>
-        <li>
-          <Link href="/bloom-filter">Bloom Filter</Link>
-        </li>
-        <li>
-          <Link href="/svg-mesh-3d">SVG Mesh 3D</Link>
-        </li>
-        <li>
-          <Link href="/text-3d">3D Text</Link>
-        </li>
-        <li>
-          <Link href="/hover-on-face">Hover on Face</Link>
-        </li>
-        <li>
-          <Link href="/iframe-positioning">
-            iframe Positioning
-          </Link>
+          Others
+          <ul>
+            <li>
+              <Link href="/decal">Decal</Link>
+            </li>
+            <li>
+              <Link href="/hover-on-face">
+                Hover on Face
+              </Link>
+            </li>
+          </ul>
         </li>
       </ul>
     </main>
